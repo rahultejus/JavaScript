a64f646b77fb65118d04388486eb045145faf4ac
Rahul Tejus <92539247+rahultejus@users.noreply.github.com>
2024-12-13 12:00:54 +0000

    conversion datatype

---
+++ 01_basics/03_dataType_conversion.js
@@ -0,0 +1,32 @@
+let score=747
+let age="9382hsj"
+
+let temp=null
+let some=undefined
+console.log(typeof score)
+
+console.log(typeof (score))
+
+const valueInNnumber=Number(score);
+
+console.log(typeof valueInNnumber)
+
+console.log(typeof age)
+
+const ageValue=Number(age)
+
+console.log(Number (temp))
+
+console.log(ageValue)
+
+console.log(Number(some))
+
+// "3848" =>3848
+// "3974sjf" =>NaN
+// true =>1 ; false =0;
+
+let isLoggedIn="rahul Kumar"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
