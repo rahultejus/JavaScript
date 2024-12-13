d3bdb8cbf3f8069e687bc151a853470694f30fc5
Rahul Tejus <92539247+rahultejus@users.noreply.github.com>
2024-12-13 10:57:59 +0000

    data types in JS

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,33 @@
+"use strict" // treat all JS code as newer version of JS
+
+// alert(4+45-34)  .// will give an error because it Node JS not browser
+
+// console.log(23+
+//     39
+// ); console.log(88+38-3873)  // always try to make it a readable code 
+
+// console.log(93+393-284);
+
+let name="Rahul Kumar"
+
+let age=22
+
+let isLoggedIn =false
+
+// premitive data type 
+
+//number => 2 power 53
+// string =>" "
+// boolean =>  true/false
+// null => standalone value
+// undefined => when the variable is only declared not initialised to anyone
+// symbol=> unique 
+
+//Non primitive data type
+
+// object 
+
+console.log(typeof null)   //object
+
+console.log(typeof undefined)  //undefined 
+
