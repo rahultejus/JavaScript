7b26fe5103553cfa784364a6cf14fb1c0e8bda65
Rahul Tejus <92539247+rahultejus@users.noreply.github.com>
2024-12-13 10:57:34 +0000

    variables in JS

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,26 @@
+const accountId=12435
+
+let accountEmail="rahul@google.com"
+
+accountCity="Lucknow"
+
+var accountPassword="249485"
+
+accountCity="Varansi"
+
+accountPassword="9384984"
+
+let accountState;
+
+// accountId=9374874  // not allowed to reassign a value to a constant 
+
+console.log(accountId);
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+
+
+/*
+  Prefer not to use the variable var 
+  because of its functional and scope issue 
+  instead use let in place of var
+*/
