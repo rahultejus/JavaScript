548ee3928f01a0beb0a70abb3ecf784df0f820b3
Rahul Tejus <92539247+rahultejus@users.noreply.github.com>
2024-12-14 07:04:56 +0000

    comparision of variables

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>3);
+// console.log(1>=2);
+// console.log(4<5);
+// console.log(4!=8);
+
+// console.log("1">2);      //Comparision should be of same data type
+// console.log("02">1);     // we need to avoid such type of comparision
+
+console.log(null>0)
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
