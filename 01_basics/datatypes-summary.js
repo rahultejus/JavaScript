2b010c9aa2a5254645d385c2e4f53c85f672d599
Rahul Tejus <92539247+rahultejus@users.noreply.github.com>
2024-12-14 07:57:42 +0000

    memory use by different data types

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,7 +34,35 @@ const myFunction = function(){
 
 // console.log(typeof anotherId);
 
-console.log(anotherId);
+// console.log(anotherId);
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// *********************** Memory use ***************************
+
+//  Stack(Primitive)  Heap(Non Primitive)
+
+
+let userName="rahul@google.com"
+let anotherUserName=userName;
+console.log(userName)
+console.log(anotherUserName)
+
+anotherUserName="rahulkumar@facebook.com"
+console.log(anotherUserName)
+console.log(userName)
+
+
+let userOne={
+    email:"rahul9373@meta.com",
+    upiId:"89038364@ybl"
+}
+
+let userTwo=userOne;
+userOne.email="rahul7777@oracle.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
