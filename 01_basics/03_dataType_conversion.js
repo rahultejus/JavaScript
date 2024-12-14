3c7861694917b8633dc6414ec661023af2c389c2
Rahul Tejus <92539247+rahultejus@users.noreply.github.com>
2024-12-14 06:14:48 +0000

    more of string conversion

--- 01_basics/03_dataType_conversion.js
+++ 01_basics/03_dataType_conversion.js
@@ -3,23 +3,23 @@ let age="9382hsj"
 
 let temp=null
 let some=undefined
-console.log(typeof score)
+// console.log(typeof score)
 
-console.log(typeof (score))
+// console.log(typeof (score))
 
 const valueInNnumber=Number(score);
 
-console.log(typeof valueInNnumber)
+// console.log(typeof valueInNnumber)
 
-console.log(typeof age)
+// console.log(typeof age)
 
 const ageValue=Number(age)
 
-console.log(Number (temp))
+// console.log(Number (temp))
 
-console.log(ageValue)
+// console.log(ageValue)
 
-console.log(Number(some))
+// console.log(Number(some))
 
 // "3848" =>3848
 // "3974sjf" =>NaN
@@ -29,4 +29,44 @@ let isLoggedIn="rahul Kumar"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+let value=3948
+
+let  valueString=String(value)
+
+// console.log(valueString)
+// console.log(typeof valueString);
+
+
+
+// ******************** Operations ***********************
+
+let valuee=944;
+let negValuee=-valuee;
+
+// console.log(negValuee);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4); // 2 power 4
+// console.log(2/2);
+// console.log(9%2);
+
+
+// console.log ( typeof ("1"+34))
+// console.log(  typeof (45+"21"))
+
+// console.log("1" + 2 + 3)
+// console.log(1+ 2 +"3")
+
+
+// console.log(true)
+// console.log(+true);
+// console.log(+"")
+
+
+
